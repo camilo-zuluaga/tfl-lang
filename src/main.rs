3ed2a3d489de4c3f08c9a166cfd8ea76eb0e4186
camilo-zuluaga <zuluagajuanca@outlook.es>
2026-07-07 18:11:41 -0500

    feat: tokenization v1

--- src/main.rs
+++ src/main.rs
@@ -45,3 +45,51 @@ enum Token {
     Implies,
     Not,
 }
+
+struct Scanner {
+    source: String,
+    tokens: Vec<Token>,
+}
+
+impl Scanner {
+    fn new(source: &str) -> Scanner {
+        Scanner {
+            source: source.to_string(),
+            tokens: Vec::new(),
+        }
+    }
+
+    fn scan_tokens(self) -> Result<Vec<Token>, LexError> {
+        let mut tokens = self.tokens;
+        let mut chars = self.source.chars().peekable();
+
+        while let Some(c) = chars.next() {
+            match c {
+                ' ' | '\t' => {}
+                '(' => tokens.push(Token::LeftParen),
+                ')' => tokens.push(Token::RightParen),
+                '~' | '!' | '¬' => tokens.push(Token::Not),
+                '&' | '∧' => tokens.push(Token::And),
+                '|' | '∨' => tokens.push(Token::Or),
+                '→' => tokens.push(Token::Implies),
+                '↔' => tokens.push(Token::Iff),
+                // i guess we should also accept <-> or ->
+                '-' => match chars.next() {
+                    Some('>') => tokens.push(Token::Implies),
+                    _ => return Err(LexError::Unexpected()),
+                },
+                // we should expect the <-> biconditional
+                '<' => match (chars.next(), chars.next()) {
+                    (Some('-'), Some('>')) => tokens.push(Token::Iff), 
+                    _ => return Err(LexError::Unexpected())
+                }
+                _ => return Err(LexError::Unexpected())
+            }
+        }
+        Ok(tokens)
+    }
+}
+
+enum LexError {
+    Unexpected(),
+}
