fe5c759315fffde4e868c93a6ca27526a087130e
camilo-zuluaga <zuluagajuanca@outlook.es>
2026-07-07 19:54:06 -0500

    feat: import lexer

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use std::{env, fs, process::ExitCode};
 
+mod lexer;
+use lexer::Scanner;
+
 fn main() -> ExitCode {
     let args: Vec<String> = env::args().collect();
 
@@ -27,69 +30,8 @@ fn run_file(path: &str) -> ExitCode {
 }
 
 fn run(source: &str) {
-    for token in source.split_whitespace() {
-        println!("{token}");
+    match Scanner::new(source).scan_tokens() {
+        Ok(tokens) => println!("{tokens:?}"),
+        Err(e) => eprintln!("lex error {e:?}"),
     }
-}
-
-/*
-will need a struct after to properly give errors
-*/
-enum Token {
-    LeftParen,
-    RightParen,
-    Atom(String),
-    And,
-    Or,
-    Iff,
-    Implies,
-    Not,
-}
-
-struct Scanner {
-    source: String,
-    tokens: Vec<Token>,
-}
-
-impl Scanner {
-    fn new(source: &str) -> Scanner {
-        Scanner {
-            source: source.to_string(),
-            tokens: Vec::new(),
-        }
-    }
-
-    fn scan_tokens(self) -> Result<Vec<Token>, LexError> {
-        let mut tokens = self.tokens;
-        let mut chars = self.source.chars().peekable();
-
-        while let Some(c) = chars.next() {
-            match c {
-                ' ' | '\t' => {}
-                '(' => tokens.push(Token::LeftParen),
-                ')' => tokens.push(Token::RightParen),
-                '~' | '!' | '¬' => tokens.push(Token::Not),
-                '&' | '∧' => tokens.push(Token::And),
-                '|' | '∨' => tokens.push(Token::Or),
-                '→' => tokens.push(Token::Implies),
-                '↔' => tokens.push(Token::Iff),
-                // i guess we should also accept <-> or ->
-                '-' => match chars.next() {
-                    Some('>') => tokens.push(Token::Implies),
-                    _ => return Err(LexError::Unexpected()),
-                },
-                // we should expect the <-> biconditional
-                '<' => match (chars.next(), chars.next()) {
-                    (Some('-'), Some('>')) => tokens.push(Token::Iff), 
-                    _ => return Err(LexError::Unexpected())
-                }
-                _ => return Err(LexError::Unexpected())
-            }
-        }
-        Ok(tokens)
-    }
-}
-
-enum LexError {
-    Unexpected(),
 }
