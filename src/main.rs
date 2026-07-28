cc4c98175199fff7592ca64c4c761c60763e95b5
camilo-zuluaga <zuluagajuanca@outlook.es>
2026-07-28 16:07:15 -0500

    feat: impl little prompt

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-use std::{env, fs, process::ExitCode};
+use std::{env, fs, io::{self, Write}, process::ExitCode};
+
+use crate::parser::Parser;
 
 mod lexer;
+mod parser;
 
 fn main() -> ExitCode {
     let args: Vec<String> = env::args().collect();
 
     match args.as_slice() {
-        [_] => ExitCode::from(64),
+        [_] => run_prompt(),
         [_, path] => run_file(path),
         _ => {
             println!("Usage: tfl [file path]");
@@ -28,9 +31,24 @@ fn run_file(path: &str) -> ExitCode {
     }
 }
 
+fn run_prompt() -> ExitCode {
+    let mut line = String::new();
+    loop {
+        print!("> ");
+        // classic buffering problem
+        io::stdout().flush().expect("flush failed");
+        line.clear();
+        io::stdin().read_line(&mut line).expect("failed to read");
+        run(&line);
+    }
+}
+
 fn run(source: &str) {
     match lexer::tokenize(source) {
-        Ok(tokens) => println!("{tokens:?}"),
+        Ok(tokens) => match Parser::new(tokens).parse() {
+            Ok(formula) => println!("{formula}"),
+            Err(e) => eprintln!("[parser error] {e}"),
+        },
         Err(e) => eprintln!("lex error {e:?}"),
     }
 }
