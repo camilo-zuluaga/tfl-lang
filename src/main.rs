f0c64e09861d0ee2df0f6d79033f6ef842c948ef
camilo-zuluaga <zuluagajuanca@outlook.es>
2026-07-29 16:16:40 -0500

    feat: adding a repl with reedline

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
-use std::{
-    env, fs,
-    io::{self, Write},
-    process::ExitCode,
+use reedline::{DefaultPrompt, Reedline, Signal};
+use std::{env, fs, process::ExitCode};
+use {
+    crossterm::event::{KeyCode, KeyModifiers},
+    reedline::{EditCommand, Emacs, ReedlineEvent, default_emacs_keybindings},
 };
 
 use crate::parser::Parser;
 
+mod eval;
 mod lexer;
 mod parser;
 
@@ -36,21 +38,41 @@ fn run_file(path: &str) -> ExitCode {
 }
 
 fn run_prompt() -> ExitCode {
-    let mut line = String::new();
-    loop {
-        print!("> ");
-        // classic buffering problem
-        io::stdout().flush().expect("flush failed");
-        line.clear();
-        io::stdin().read_line(&mut line).expect("failed to read");
-        run(&line);
+    // just getting it done
+    let mut kb = default_emacs_keybindings();
+    for (ch, sym) in [('a', "∧"), ('o', "∨"), ('n', "¬"), ('i', "→"), ('b', "↔")] {
+        kb.add_binding(
+            KeyModifiers::CONTROL,
+            KeyCode::Char(ch),
+            ReedlineEvent::Edit(vec![EditCommand::InsertString(sym.to_string())]),
+        );
     }
+    let edit_mode = Box::new(Emacs::new(kb));
+    let mut line_editor = Reedline::create().with_edit_mode(edit_mode);
+    let prompt = DefaultPrompt::default();
+
+    loop {
+        let sig = line_editor.read_line(&prompt);
+        match sig {
+            Ok(Signal::Success(buffer)) => {
+                println!("We processed: {}", buffer);
+            }
+            Ok(Signal::CtrlD) | Ok(Signal::CtrlC) => {
+                println!("\nAborted!");
+                break;
+            }
+            x => {
+                println!("Event: {:?}", x);
+            }
+        }
+    }
+    ExitCode::SUCCESS
 }
 
 fn run(source: &str) {
     match lexer::tokenize(source) {
         Ok(tokens) => match Parser::new(tokens).parse() {
-            Ok(formula) => println!("{formula}"),
+            Ok(formula) => eval::truth_table(&formula),
             Err(e) => eprintln!("[parser error] {e}"),
         },
         Err(e) => eprintln!("lex error {e:?}"),
